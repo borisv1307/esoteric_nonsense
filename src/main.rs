fbc355c05a0531fd33b8b722bbd14608e87b7a86
TPXS-Drexel <71519259+TPXS-Drexel@users.noreply.github.com>
2020-10-23 16:14:03 -0400

    I accidentally an argument

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,23 @@ use std::os::raw::{c_char};
 mod s_y;
 
 
+
 fn main() {
  
     let mut s: s_y::ShuntingYard = s_y::ShuntingYard::new();
     assert_eq!(s.calculate("2 + 3").unwrap(), 5.0);
     let r: Result<f32, Vec<String>> = s.calculate("2 + 4");
     println!("{:?}", r);
-  
+    let t = make_all_x_strings("x^3".to_string(), -10.0, 10.0, 1.0);
+    println!("{:?}", t);
+    
 }
 
 pub fn infix_calculator(expression: String) -> f32 {
- 
     let mut s: s_y::ShuntingYard = s_y::ShuntingYard::new();
     assert_eq!(s.calculate("2 + 3").unwrap(), 5.0);
     let r: Result<f32, Vec<String>> = s.calculate(&expression);
     r.unwrap() 
- 
 } 
 
 pub fn make_all_x_strings (expression: String, x_lower: f32, x_upper: f32, x_precision: f32) -> Vec<String> {
@@ -49,7 +50,7 @@ pub fn func_of_x (expression: String, x_lower: f32, x_upper: f32, x_precision: f
 }
 
 
-// changed xsquared to func_of_x, add expression to arguments 
+// changed xsquared to func_of_x, add expression to arguments (//TODO: strings over ffi??)
 #[no_mangle]
 pub unsafe extern "C" fn coord_vector_maker (input: *const c_char, x_lower: f32, x_upper: f32, y_lower: f32, y_upper: f32, x_precision: f32, y_precision: f32) ->  *mut CoordPair<f32> {
     let input_c_str: &CStr = unsafe { CStr::from_ptr(input)};
@@ -57,7 +58,7 @@ pub unsafe extern "C" fn coord_vector_maker (input: *const c_char, x_lower: f32,
     let expression: String = str_slice.to_owned(); 
     
     let x_all : Vec<f32> = x_vector_maker(x_lower, x_upper, x_precision);
-    let y_all : Vec<f32> = round_y_to_precision(func_of_x(expression: String, x_lower: f32, x_upper: f32, x_precision: f32, y_lower: f32, y_upper: f32));
+    let y_all : Vec<f32> = round_y_to_precision(func_of_x(expression, x_lower, x_upper, x_precision, y_lower, y_upper), y_precision);
     let x_ptr = x_all.as_ptr();
     let y_ptr = y_all.as_ptr();
     let c_out = CoordPair::new(x_ptr, y_ptr);
